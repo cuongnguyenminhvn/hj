5accb07db02a86287804e13d58c89a894a5dc740
cuongnguyenminhvn <cuongnguyenminhvn@gmail.com>
2021-09-02 15:44:55 -0400

    Update pham.sh

--- pham.sh
+++ pham.sh
@@ -4,5 +4,5 @@ sudo apt install libpci3
 sudo wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
 tar -xf lolMiner_v1.29_Lin64.tar.gz
 cd 1.29
-sudo ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user 0xc8dada213812ef9173c89bf7f46e86a0968b3367.haithanh04 --worker $(echo $(shuf -i 201-400 -n 1)-$(shuf -i 1-200 -n 1)-$(shuf -i 401-600 -n 1)-$(shuf -i 601-800 -n 1))
+sudo ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user ETH:0xc8dada213812ef9173c89bf7f46e86a0968b3367.cuong01 --ethstratum ETHPROXY
 sleep 99999 
